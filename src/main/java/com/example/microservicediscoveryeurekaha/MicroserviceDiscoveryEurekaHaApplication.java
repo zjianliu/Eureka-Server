d697c88b95f3f7cf9c679c6911692e024bb8355d
zjianliu <1136372287@qq.com>
2018-12-02 13:35:26 +0800

    Eureka Server

---
+++ src/main/java/com/example/microservicediscoveryeurekaha/MicroserviceDiscoveryEurekaHaApplication.java
@@ -0,0 +1,14 @@
+package com.example.microservicediscoveryeurekaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceDiscoveryEurekaHaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDiscoveryEurekaHaApplication.class, args);
+	}
+}
